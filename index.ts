383ea4e34ddf67e1f6bf0ec269f4d8bc936c5426
Roshangautam2052 <106965552+Roshangautam2052@users.noreply.github.com>
2025-07-31 11:27:28 +0100

    Initialised repository

---
+++ index.ts
@@ -0,0 +1,18 @@
+const students = [
+    {
+        name: "Roshan",
+        score: 70
+    }, {
+        name: "Roshan",
+        score: 70
+    },
+    {
+        name: "Roshan",
+        score: 70
+    }
+];
+
+for(const student of students){
+    console.log("Name:", student.name);
+    console.log("Score", student.score);
+}
